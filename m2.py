ae9cd930ab40acd302ce4a2621a860d3318ac06c
deadpix3l <skylerr.curtis@gmail.com>
2015-07-02 14:13:51 +0000

    semi stable. needs input functions

---
+++ m2.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import random
+
+class Markov(object):
+    def __init__(self, story_file, order=2, letters = 0):
+        self.story_file = story_file
+        self.order = order
+        self.letters = letters
+        self.words = self.generateWords()
+        self.model = self.createModel()
+        
+        
+    def generateWords(self):
+        with open(self.story_file) as story:
+            data = story.read()
+            #if letters = 1, split into letters, else, split to words.
+            if (self.letters): words = list(data)
+            else: words = data.split()
+        return words
+        
+    def triples(self, size=2):
+        if len(self.words) < size+1: return
+        for i in range(len(self.words)-size):
+            x = []
+            for off in range(size+1):
+                x.append(self.words[i+off])
+            yield tuple(x)
+            
+    def createModel(self):
+        model = {}
+        for x in self.triples(self.order):
+            key = x[:-1]
+            if key in model: model[key].append(x[-1]) #append word to list
+            else: model[key] = [x[-1]] #brackets ensure its initialized as a list
+        return model
+        
+    def createStory(self, size=20):
+        seed = random.randint(0, len(self.words)-(self.order+2))
+        
+        currentkey = []
+        for off in range(self.order):
+            currentkey.append(self.words[seed+off])#seed words
+        generatedtext = []
+        for i in range(size+1):
+            generatedtext.append(currentkey[0])
+            currentkey.append(random.choice(self.model[tuple(currentkey)]))
+            currentkey = currentkey[1:]
+        delim = '' if self.letters else ' '
+        return delim.join(generatedtext+currentkey)
+
+
+testing = Markov('text/pride.txt', 5, 1)
+#print testing.model.items()[1:10]
+print testing.createStory(400)
